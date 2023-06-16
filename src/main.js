f7a533680c06669e62b7e41ade128e730a0bed40
AndrewKushnir <cuckop30@gmail.com>
2023-06-16 20:24:51 +0300

    Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Presenter from './presenter/presenter';
 import FiltersView from './view/FiltersView';
 import TripModel from './model/tripModel';
+import { generateFilter } from './mocks/mock';
 import { render } from './framework/render';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
-render(new FiltersView(), filtersContainer);
-
 const tripModel = new TripModel();
+
+const filters = generateFilter(tripModel.points);
+render(new FiltersView(filters), filtersContainer);
+
 const container = document.querySelector('.trip-events');
 const tripPresenter = new Presenter(container, tripModel);
 
